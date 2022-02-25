64deabff188471ee86cbd90a156471de22f832cc
François <mockersf@gmail.com>
2022-02-25 23:22:09 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,20 @@ fn main() {
 }
 
 fn hello_world(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-    let text_style = TextStyle {
-        font,
-        font_size: 60.0,
-        color: Color::WHITE,
-    };
-    let text_alignment = TextAlignment {
-        vertical: VerticalAlign::Center,
-        horizontal: HorizontalAlign::Center,
-    };
-    // 2d camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    // Demonstrate changing translation
     commands.spawn_bundle(Text2dBundle {
-        text: Text::with_section("Cleanse", text_style.clone(), text_alignment),
+        text: Text::with_section(
+            "Cleanse",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 60.0,
+                color: Color::WHITE,
+            },
+            TextAlignment {
+                vertical: VerticalAlign::Center,
+                horizontal: HorizontalAlign::Center,
+            },
+        ),
         ..Default::default()
     });
 }
