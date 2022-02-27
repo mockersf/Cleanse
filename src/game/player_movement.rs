06dd2fea92f7c52fc715307ca10e15cfc61e3a8a
François <mockersf@gmail.com>
2022-02-27 05:24:59 +0100

    do not go under UI

--- src/game/player_movement.rs
+++ src/game/player_movement.rs
@@ -26,8 +26,12 @@ pub fn player_movements(
         let move_by = order.normalize().extend(0.0) * time.delta_seconds();
         player.0.translation += move_by * player.1.speed;
         let window = windows.get_primary().unwrap();
-        let (width, height) = (window.width() * 0.975, window.height() * 0.975);
+        let (width, height) = (window.width() * 0.985, window.height() * 0.975);
         player.0.translation.x = player.0.translation.x.clamp(-width / 2.0, width / 2.0);
-        player.0.translation.y = player.0.translation.y.clamp(-height / 2.0, height / 2.0);
+        player.0.translation.y = player
+            .0
+            .translation
+            .y
+            .clamp(-height / 2.0, height / 2.0 * 0.925);
     }
 }
