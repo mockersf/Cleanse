6397923a7d790b5e482c8391c09c6940c739d3c3
François <mockersf@gmail.com>
2022-02-27 03:27:55 +0100

    limit movements to window

--- src/game/player_movement.rs
+++ src/game/player_movement.rs
@@ -6,6 +6,7 @@ pub fn player_movements(
     keyboard_input: Res<Input<KeyCode>>,
     time: Res<Time>,
     mut player: Query<(&mut Transform, &Player)>,
+    windows: Res<Windows>,
 ) {
     let mut order = Vec2::ZERO;
     if keyboard_input.pressed(KeyCode::Right) {
@@ -24,5 +25,9 @@ pub fn player_movements(
         let mut player = player.single_mut();
         let move_by = order.normalize().extend(0.0) * time.delta_seconds();
         player.0.translation += move_by * player.1.speed;
+        let window = windows.get_primary().unwrap();
+        let (width, height) = (window.width() * 0.975, window.height() * 0.975);
+        player.0.translation.x = player.0.translation.x.clamp(-width / 2.0, width / 2.0);
+        player.0.translation.y = player.0.translation.y.clamp(-height / 2.0, height / 2.0);
     }
 }
