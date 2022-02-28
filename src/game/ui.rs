0e6e827e8846c533f7dfbd6c7ff905dac34b13ea
François <mockersf@gmail.com>
2022-02-28 01:37:57 +0100

    health bar

--- src/game/ui.rs
+++ src/game/ui.rs
@@ -4,9 +4,13 @@ use bevy_egui::{
     EguiContext,
 };
 
-use super::HostState;
+use super::{immune_system::ImmuneSystem, HostState};
 
-pub fn status(mut egui_context: ResMut<EguiContext>, state: Res<HostState>) {
+pub fn status(
+    mut egui_context: ResMut<EguiContext>,
+    state: Res<HostState>,
+    immune_system: Query<&ImmuneSystem>,
+) {
     egui::TopBottomPanel::top("top").show(egui_context.ctx_mut(), |ui| {
         let mut status_to_display = LayoutJob::default();
 
@@ -29,6 +33,34 @@ pub fn status(mut egui_context: ResMut<EguiContext>, state: Res<HostState>) {
                     background: Color32::RED,
                     ..Default::default()
                 },
+            },
+        );
+
+        status_to_display.append(
+            " ",
+            0.0,
+            TextFormat::simple(egui::TextStyle::Body, Color32::GRAY),
+        );
+
+        let immune_system = immune_system.single();
+        status_to_display.append(
+            &"-".repeat((immune_system.health / immune_system.original_health * 50.0) as usize),
+            0.0,
+            TextFormat {
+                color: Color32::GREEN,
+                background: Color32::GREEN,
+                ..Default::default()
+            },
+        );
+        status_to_display.append(
+            &"-".repeat(
+                ((1.0 - immune_system.health / immune_system.original_health) * 50.0) as usize,
+            ),
+            0.0,
+            TextFormat {
+                color: Color32::RED,
+                background: Color32::RED,
+                ..Default::default()
             },
         );
 
